91130f54bc8678b2acb7bd01f0573a7c273dff3a
Francis David <fransisco.david15@gmail.com>
2018-08-23 16:40:01 -0500

    se agrego un archivo Sumaaa.php

---
+++ Sumaaa.php
@@ -0,0 +1,12 @@
+<?php
+	if($_POST)
+	{
+	$numl = $_POST
+	['Numero 1'];
+	$num2 = $_POST
+	['Numero 2'];
+	$suma = $numl
+	+$num2;
+	echo"La suma de".$numl."y la de ".$num2."La suma es de ;".$suma
+	}
+?>
